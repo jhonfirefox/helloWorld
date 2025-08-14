9340fec472ffa08a8d748d3db02c6866940b9f28
jhonfirefox <39972493+jhonfirefox@users.noreply.github.com>
2025-08-14 11:17:02 -0500

    Se anexa una nueva funcion para sumar dos numeros

--- helloGit.py
+++ helloGit.py
@@ -1,6 +1,9 @@
 def printHola(name):
     print("Hola: "+name)
+def suma2(a,b):
+    return a+b
 
 print("new Hello Git")
 print("Ahora estoy editando mi fichero")
 printHola(jhon)
+print("1+2 = ", suma2(1+2))
